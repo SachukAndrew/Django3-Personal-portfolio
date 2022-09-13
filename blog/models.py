c487e15c4b180303b4a070325fae822e027a2cf0
sachukandrew <sach1985@mail.ru>
2022-09-13 09:41:42 +0300

    blog and portfolio without link and css styles

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class Message(models.Model):
     title = models.CharField(max_length=200)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
     description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from . import Message
+from .models import Message
 
 def all_blogs(request):
-    blogs = Message.objects.all()
+    # blog_count = Message.objects.count() - так можно создать переменную для подсчета постов
+    blogs = Message.objects.order_by('-date')[0:5]
     return render(request, 'blog/all_blogs.html', {"blogs": blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Message, pk=blog_id)
+    return render(request, 'blog/detail.html', {"blog": blog})
